526da2c144095b1c08f26be124b473fc3c2c375a
juanloza <juan.loza@gmx.com>
2023-06-16 22:05:54 +0200

    variables-class

---
+++ main.js
@@ -0,0 +1,13 @@
+/* console.log("mi primer script");
+
+console.log("este es otro por si acaso");*/
+
+
+
+let height = 180;
+let anotherHeight= height;
+let weight;
+console.log(height);
+console.log(anotherHeight);
+weight = 70;
+console.log(weight);
